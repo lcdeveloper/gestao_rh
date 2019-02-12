0d065dce8727203033cc266deb520032eb886fbb
luiz <luizclaudio.jau@gmail.com>
2019-02-12 21:11:02 -0200

    Seção 2, aula 11

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from apps.funcionarios.models import Funcionario
+
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100, help_text="Descrição do documento")
+    proprietario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.descricao
