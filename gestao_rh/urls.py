320b6361337cdd5d0ec9a50b2e8942eda21e5e51
luiz <luizclaudio.jau@gmail.com>
2019-02-13 20:57:13 -0200

    Seção 4, aula 17

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('apps.core.urls')),
-    path('funcionarios/', include('apps.funcionarios.urls')),
+    path('funcionario/', include('apps.funcionarios.urls')),
+    path('empresa/', include('apps.empresas.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
